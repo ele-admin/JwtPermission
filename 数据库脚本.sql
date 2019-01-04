ce15e04aa761182151699c0e8788e8159c91b28b
synchronized <whvcse@foxmail.com>
2019-01-04 16:03:27 +0800

    Update 数据库脚本.sql

--- 数据库脚本.sql
+++ 数据库脚本.sql
@@ -1,7 +1,10 @@
+
+DROP TABLE IF EXISTS `oauth_token`;
+
 CREATE TABLE `oauth_token` (
   `token_id` int(11) NOT NULL AUTO_INCREMENT,
-  `access_token` varchar(128) DEFAULT NULL,
-  `user_id` varchar(128) DEFAULT NULL,
+  `access_token` varchar(128) NOT NULL,
+  `user_id` varchar(128) NOT NULL,
   `permissions` varchar(512) DEFAULT NULL,
   `roles` varchar(512) DEFAULT NULL,
   `refresh_token` varchar(128) DEFAULT NULL,
@@ -9,11 +12,14 @@ CREATE TABLE `oauth_token` (
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`token_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `oauth_token_key`;
 
 CREATE TABLE `oauth_token_key` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `token_key` varchar(128) DEFAULT NULL COMMENT '生成token时的key',
+  `token_key` varchar(128) NOT NULL COMMENT '生成token时的key',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
